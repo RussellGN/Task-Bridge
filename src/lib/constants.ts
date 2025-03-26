862a20c2cd5e6e4f5679bc86142c826a703213a4
Russellg <dudurussell@gmail.com>
2025-03-26 17:01:57 +0200

    feat: implement Signin screen

---
+++ src/lib/constants.ts
@@ -0,0 +1,1 @@
+export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}`;
--- src/routes/Signin.tsx
+++ src/routes/Signin.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { GITHUB_AUTH_URL } from "@/lib/constants";
+import { LogIn, UserCircle } from "lucide-react";
+import { Link } from "react-router";
+
 export default function Signin() {
-   return <div>signin</div>;
+   return (
+      <div className="pt-30 flex justify-center items-center flex-col gap-4 text-center">
+         <UserCircle size={100} />
+
+         <p>
+            You need to sign into your GitHub account <br /> in order to proceed
+         </p>
+
+         <Button asChild className="btn btn-primary">
+            <Link to={GITHUB_AUTH_URL} target="_blank">
+               Signin with Github
+               <LogIn />
+            </Link>
+         </Button>
+      </div>
+   );
 }
