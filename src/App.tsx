c7a3059e136cc42c63eb6f9e0f2598f1f942719c
Russellg <dudurussell@gmail.com>
2025-03-27 08:27:14 +0200

    feat: add Home route and component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Signin from "./routes/Signin";
 import Layout from "./components/general/Layout";
+import Home from "./routes/Home";
 export default function App() {
    return (
       <BrowserRouter>
          <Routes>
             <Route Component={Layout}>
                <Route path="/" Component={Signin} />
+               <Route path="/home" Component={Home} />
             </Route>
          </Routes>
       </BrowserRouter>
