52a0e39198bffba830cd05cd2088da60a46e4379
Russellg <dudurussell@gmail.com>
2025-03-27 16:34:35 +0200

    feat: add debugging function for store entries in utils

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,3 +1,14 @@
+use tauri::Runtime;
+use tauri_plugin_store::Store;
+
 pub fn log(msg: impl Into<String>) {
    println!("{}", msg.into());
+}
+
+pub fn dbg_store(store: &Store<impl Runtime>) {
+   let store = store.entries();
+
+   println!("-------------store debug--------------");
+   println!("{store:#?}");
+   println!("--------------------------------------");
 }
