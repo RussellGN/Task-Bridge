a34812a7f601c75e3f0c48f119ef3538d5cc8214
Russellg <dudurussell@gmail.com>
2025-03-27 10:18:27 +0200

    feat: enhance GITHUB_AUTH_URL with prompt and state parameters using a random global auth keyword

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,3 @@
-export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}`;
+import { random_global_auth_keyword } from "./utils";
+
+export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&prompt=select_account&state=${random_global_auth_keyword()}`;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
    return twMerge(clsx(inputs));
 }
+
+export function random_global_auth_keyword() {
+   return "key_t_b_auth";
+}
