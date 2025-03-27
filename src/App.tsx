9a1a312f7cd9a8fd7397f286c9aa66c50e32bedc
Russellg <dudurussell@gmail.com>
2025-03-27 16:35:06 +0200

    refactor: add a blank line for improved code readability

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Signin from "./routes/Signin";
 import Layout from "./components/general/Layout";
 import Home from "./routes/Home";
+
 export default function App() {
    return (
       <BrowserRouter>
