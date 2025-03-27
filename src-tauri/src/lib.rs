ae18cfd2b031db7b3738af8a28d33ce34e3238cc
Russellg <dudurussell@gmail.com>
2025-03-27 09:32:26 +0200

    feat: set up tauri-deep-link plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+mod setup;
+mod utils;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -7,8 +10,10 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
    tauri::Builder::default()
+      .plugin(tauri_plugin_deep_link::init())
       .plugin(tauri_plugin_store::Builder::new().build())
       .plugin(tauri_plugin_opener::init())
+      .setup(setup::setup)
       .invoke_handler(tauri::generate_handler![greet])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
---
+++ src-tauri/src/setup.rs
@@ -0,0 +1,46 @@
+use std::error::Error;
+
+use tauri::{App, Runtime};
+use tauri_plugin_deep_link::DeepLinkExt;
+
+use crate::utils::log;
+
+pub fn setup(app: &mut App<impl Runtime>) -> Result<(), Box<dyn Error>> {
+   let deep_link = app.deep_link();
+
+   match deep_link.is_registered("task-bridge") {
+      Err(e) => {
+         log(format!(
+            "could not confirm if app is registered to handle 'task-bridge' urls. Error: {e}"
+         ));
+         log("attempting to register");
+
+         match deep_link.register_all() {
+            Ok(_) => {
+               log("registration as 'task-bridge' handler successfull");
+               log(format!(
+                  "[deep_link.is_registered] {:#?}",
+                  deep_link.is_registered("task-bridge")
+               ));
+            }
+            Err(e) => log(format!(
+               "failed to register app to handle 'task-bridge' urls. Error: {e}"
+            )),
+         }
+      }
+
+      Ok(is_reg) => {
+         log(format!("is app registered to handle 'task-bridge' urls? {is_reg}"));
+      }
+   }
+
+   deep_link.on_open_url(|e| {
+      log(format!("deep link hit [on_open_url]. Urls: {:#?}", e.urls()));
+   });
+
+   if let Ok(Some(urls)) = deep_link.get_current() {
+      log(format!("app loaded by deep link, urls: {urls:#?}"));
+   }
+
+   Ok(())
+}
