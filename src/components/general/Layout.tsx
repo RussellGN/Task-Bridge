9a637cf0487665b6b0cfc1c614725d6074952f9d
Russellg <dudurussell@gmail.com>
2025-03-27 08:27:41 +0200

    feat: add temporary navigation links for ease of development

--- src/components/general/Layout.tsx
+++ src/components/general/Layout.tsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
+import { Button } from "../ui/button";
 
 export default function Layout() {
    return (
-      <>
+      <div>
+         <nav className="">
+            <Button asChild variant="link">
+               <Link className="" to="/home">
+                  Home
+               </Link>
+            </Button>
+            <Button asChild variant="link">
+               <Link className="" to="/">
+                  Sign In
+               </Link>
+            </Button>
+         </nav>
          <Outlet />
-      </>
+      </div>
    );
 }
