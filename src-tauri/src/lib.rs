cf9dff1d4d5a750b023de5522daea291046fa213
Russellg <dudurussell@gmail.com>
2025-03-27 09:38:09 +0200

    refactor: remove unused greet command and create commands module

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,11 +1,6 @@
+mod commands;
 mod setup;
 mod utils;
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-   format!("Hello, {}! You've been greeted from Rust!", name)
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -14,7 +9,6 @@ pub fn run() {
       .plugin(tauri_plugin_store::Builder::new().build())
       .plugin(tauri_plugin_opener::init())
       .setup(setup::setup)
-      .invoke_handler(tauri::generate_handler![greet])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
 }
