8512657729bac2d19851238395b054b1d77e540c
Russellg <dudurussell@gmail.com>
2025-03-27 16:36:01 +0200

    feat: add STORE_PATH constant for frontend usage

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,5 @@
 import { random_global_auth_keyword } from "./utils";
 
 export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&prompt=select_account&state=${random_global_auth_keyword()}`;
+
+export const STORE_PATH = "store.json";
