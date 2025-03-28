52a065c7ef6d1248f1f6e3e03698c54338fc684e
Russellg <dudurussell@gmail.com>
2025-03-28 09:45:34 +0200

    feat: update GITHUB_AUTH_URL to include redirect_uri

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 import { random_global_auth_keyword } from "./utils";
 
-export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&prompt=select_account&state=${random_global_auth_keyword()}`;
+export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&prompt=select_account&state=${random_global_auth_keyword()}&redirect_uri=${import.meta.env.VITE_PROCEED_TO_AUTH_URI}`;
 
 export const STORE_PATH = "store.json";
