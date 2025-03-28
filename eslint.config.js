fd9180c73d8117b624d80ddfd0746f3a20d917e3
Russellg <dudurussell@gmail.com>
2025-03-28 10:29:29 +0200

    chore: ignore src-tauri during esl-linting; auto detect reatc version; disable jsx-in-scope rule

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,21 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 export default defineConfig([
-   { files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-   { files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-   { files: ["src/**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+   { ignores: ["src-tauri/"] },
+   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
+   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
    tseslint.configs.recommended,
    pluginReact.configs.flat.recommended,
+   {
+      settings: {
+         react: {
+            version: "detect",
+            jsxRuntime: "automatic",
+         },
+      },
+      rules: {
+         "react/react-in-jsx-scope": "off",
+      },
+   },
 ]);
