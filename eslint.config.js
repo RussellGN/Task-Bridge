28dbf109aa6a7b43107ece970f454e9bb1738f3c
Russellg <dudurussell@gmail.com>
2025-04-02 15:02:41 +0200

    feat: setup React Query, with dev tools and eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,8 +3,10 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default defineConfig([
+   ...pluginQuery.configs["flat/recommended"],
    { ignores: ["src-tauri/"] },
    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
    { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router";
 import Layout from "./components/general/Layout";
 import { ROUTES } from "./lib/constants";
+import ReactQueryProvider from "./providers/ReactQueryProvider";
 
 export default function App() {
    return (
-      <BrowserRouter>
-         <Routes>
-            <Route Component={Layout}>
-               {ROUTES.map((route) => (
-                  <Route key={route.path} path={route.path} Component={route.component} />
-               ))}
-            </Route>
-         </Routes>
-      </BrowserRouter>
+      <ReactQueryProvider>
+         <BrowserRouter>
+            <Routes>
+               <Route Component={Layout}>
+                  {ROUTES.map((route) => (
+                     <Route key={route.path} path={route.path} Component={route.component} />
+                  ))}
+               </Route>
+            </Routes>
+         </BrowserRouter>
+      </ReactQueryProvider>
    );
 }
