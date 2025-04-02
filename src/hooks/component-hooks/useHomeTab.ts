bdfb5f1f7584b488dfa412dbec29cf8bc53679f4
Russellg <dudurussell@gmail.com>
2025-04-02 15:20:07 +0200

    feat: fetch app version using Tauri API

--- src/hooks/component-hooks/useHomeTab.ts
+++ src/hooks/component-hooks/useHomeTab.ts
@@ -1,5 +1,15 @@
+import { logError } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { getVersion } from "@tauri-apps/api/app";
+
 export default function useHomeTab() {
-   const appVersion = "0.0.1";
+   const [appVersion, setAppVersion] = useState("");
+
+   useEffect(() => {
+      getVersion()
+         .then(setAppVersion)
+         .catch((e) => logError("[useHomeTab] Error fetching app version: " + e));
+   }, []);
 
    return { appVersion };
 }
