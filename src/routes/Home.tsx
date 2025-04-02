a2b0d96c5954c37fc203d031015cbe29a51e8b66
Russellg <dudurussell@gmail.com>
2025-04-02 11:52:20 +0200

    feat: add shadcn tabs; implement tabs in home screen

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,31 @@
 import useHome from "@/hooks/route-hooks/useHome";
 import DefaultTab from "@/components/screens/home/DefaultTab";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
    const { user, loading, error } = useHome();
+   const TABS = [{ name: "Default", Component: <DefaultTab loading={loading} error={error} user={user} /> }];
 
    return (
       <div>
+         <Tabs defaultValue={TABS[0].name} className="w-[400px]">
+            <TabsList>
+               {TABS.map((tab) => (
+                  <TabsTrigger key={tab.name} value={tab.name}>
+                     {tab.name}
+                  </TabsTrigger>
+               ))}
+            </TabsList>
+
+            <div>
+               {TABS.map((tab) => (
+                  <TabsContent key={tab.name} value={tab.name}>
+                     {tab.Component}
+                  </TabsContent>
+               ))}
+            </div>
+         </Tabs>
+
          <DefaultTab loading={loading} error={error} user={user} />
       </div>
    );
