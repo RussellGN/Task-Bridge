533f9d2e346c27f5e72463c3a6f6a9974c870ef4
Russellg <dudurussell@gmail.com>
2025-04-04 08:25:18 +0200

    feat: implement rate limiting for user search queries to comply with GitHub API limits

--- src/hooks/backend-api-hooks/useSearchUsers.ts
+++ src/hooks/backend-api-hooks/useSearchUsers.ts
@@ -1,10 +1,14 @@
 import { AuthorInterface } from "@/lib/interfaces";
-import { logInfo } from "@/lib/utils";
+import { alertError, logInfo } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api/core";
+import { useState } from "react";
 
 /** get query matching users from cache, or github if not found. */
 export default function useSearchUsers(query: string) {
+   const [lastFetchHappenedAt, setLastFetchHappenedAt] = useState(0);
+   const MINIMUM_ALLOWABLE_FETCH_INTERVAL_IN_MS = 7500; // 7.5 seconds (8 times per minute)
+
    const { data, error, isLoading, refetch } = useQuery<AuthorInterface[], string>({
       queryKey: ["users", query],
       queryFn: async () => {
@@ -13,5 +17,17 @@ export default function useSearchUsers(query: string) {
       },
    });
 
-   return { loading: isLoading, queriedUsers: data, error, startSearch: refetch };
+   function rateLimitedRefetch() {
+      const now = Date.now();
+      if (now - lastFetchHappenedAt >= MINIMUM_ALLOWABLE_FETCH_INTERVAL_IN_MS) {
+         setLastFetchHappenedAt(now);
+         refetch();
+      } else {
+         alertError(
+            "[rateLimitedRefetch] Search aborted to respect GitHub rate limits (10 searches per minute). Please wait a few seconds before trying again.",
+         );
+      }
+   }
+
+   return { loading: isLoading, queriedUsers: data, error, startSearch: rateLimitedRefetch };
 }
