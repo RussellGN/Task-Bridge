2973cd64e8c1405c6ad3f3bb6416fad43fdd1160
Russellg <dudurussell@gmail.com>
2025-04-08 10:37:00 +0200

    refactor: extract store and token retrieval into separate functions

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,19 +1,37 @@
+use std::sync::Arc;
+
 use octocrab::models;
 use tauri::{AppHandle, Runtime};
-use tauri_plugin_store::StoreExt;
+use tauri_plugin_store::{Store, StoreExt};
 
 use crate::{auth::AccessToken, github_api, log, utils::dbg_store, STORE_PATH};
+
+fn get_store<R: Runtime>(app: AppHandle<R>) -> crate::Result<Arc<Store<R>>> {
+   const F: &str = "[get_store]";
+
+   log!("{F} getting store");
+   let store = app.store(STORE_PATH).map_err(|e| format!("{F} {}", e.to_string()))?;
+
+   Ok(store)
+}
+
+fn get_token<R: Runtime>(store: &Arc<Store<R>>) -> crate::Result<AccessToken> {
+   const F: &str = "[get_token]";
+
+   log!("{F} getting token");
+   let token = store.get("token").ok_or(format!("{F} token not found"))?;
+   log!("{F} token value: {token:#?}");
+   let token = serde_json::from_value::<AccessToken>(token).map_err(|e| e.to_string())?;
+
+   Ok(token)
+}
 
 #[tauri::command]
 pub async fn fetch_save_and_return_user<R: Runtime>(app: AppHandle<R>) -> crate::Result<models::Author> {
    const F: &str = "[fetch_save_and_return_user]";
 
-   log!("{F} getting store");
-   let store = app.store(STORE_PATH).map_err(|e| e.to_string())?;
-   log!("{F} getting token");
-   let token = store.get("token").ok_or(format!("{F} token not found"))?;
-   log!("{F} token value: {token:#?}");
-   let token = serde_json::from_value::<AccessToken>(token).map_err(|e| e.to_string())?;
+   let store = get_store(app)?;
+   let token = get_token(&store)?;
    let user = github_api::get_user(&token.get_token()).await?;
    let user_val = serde_json::to_value(&user).map_err(|e| e.to_string())?;
    log!("{F} setting user: {user_val:#?}");
@@ -29,14 +47,8 @@ pub async fn find_users_matching_query<R: Runtime>(
    app: AppHandle<R>,
    query: String,
 ) -> crate::Result<Vec<models::Author>> {
-   const F: &str = "[find_users_matching_query]";
-
-   log!("{F} getting store");
-   let store = app.store(STORE_PATH).map_err(|e| e.to_string())?;
-   log!("{F} getting token");
-   let token = store.get("token").ok_or(format!("{F} token not found"))?;
-   log!("{F} token value: {token:#?}");
-   let token = serde_json::from_value::<AccessToken>(token).map_err(|e| e.to_string())?;
+   let store = get_store(app)?;
+   let token = get_token(&store)?;
    let users_found = github_api::search_users(&query, &token.get_token()).await?;
    Ok(users_found)
 }
