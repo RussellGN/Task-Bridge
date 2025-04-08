f8c466fb5bdb40a312d366fcf55b085beff54d03
Russellg <dudurussell@gmail.com>
2025-04-08 15:21:37 +0200

    feat:  display current pathname in NotFound, when in development mode

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,14 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { CircleArrowLeft, Home } from "lucide-react";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 
 export default function NotFound() {
+   const { pathname } = useLocation();
+
    return (
       <div className="pt-30">
          <h1 className="text-center text-xl font-bold">Something Went Wrong...</h1>
 
          <p className="text-muted-foreground my-4 text-center">
             Not Found - This part of the app does not exist (yet).
+            <br />
+            {import.meta.env.DEV && <span className="text-red-500"> {pathname}</span>}
          </p>
 
          <div className="flex items-center justify-center gap-3">
