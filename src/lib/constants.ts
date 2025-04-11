d2a8bc8912e61f5f271380acefeaa7dd5a711d88
Russellg <dudurussell@gmail.com>
2025-04-11 15:08:54 +0200

    fix: set 'GITHUB_AUTH_URL' to app-installation page, not app-authorization page

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ import Home from "@/routes/Home";
 import NotFound from "@/routes/NotFound";
 import ProjectDashboard from "@/routes/ProjectDashboard";
 
-export const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_CLIENT_ID}&prompt=select_account&state=${random_global_auth_keyword()}&redirect_uri=${import.meta.env.VITE_PROCEED_TO_AUTH_URI}`;
+export const GITHUB_AUTH_URL = `https://github.com/apps/Task-Bridge/installations/new?prompt=select_account&state=${random_global_auth_keyword()}`;
 
 export const STORE_PATH = "store.json";
 
