04f6d3cb392cf2d644bd8a1848a5340465dd8483
Russellg <dudurussell@gmail.com>
2025-04-11 12:53:17 +0200

    feat: display repo creation date in ProjectCard

--- src/components/screens/home/ProjectCard.tsx
+++ src/components/screens/home/ProjectCard.tsx
@@ -3,6 +3,11 @@ import { FolderOpen } from "lucide-react";
 import { Link } from "react-router";
 
 export default function ProjectCard({ project }: { project: Project }) {
+   const shortDate = new Date(project.repo.created_at).toLocaleDateString("en-GB", {
+      year: "2-digit",
+      month: "short",
+      day: "2-digit",
+   });
    return (
       <Link to={`/project-dashboard/${project.id}`} className={`w-[30%] p-1 text-center sm:w-[20%] md:w-[16.66%]`}>
          <div className="hover:border-foreground/20 rounded-md border border-transparent p-3 transition-all">
@@ -12,6 +17,7 @@ export default function ProjectCard({ project }: { project: Project }) {
                </span>
             </div>
             <p className="line-clamp-1 font-semibold">{project.name}</p>
+            <p className="text-foreground/50 line-clamp-1 text-xs">{shortDate}</p>
          </div>
       </Link>
    );
