572837ee9aafedfc08f1015a1f4fc5eee0ce70f9
Russellg <dudurussell@gmail.com>
2025-04-16 12:40:23 +0200

    fix, feat: hide devkit instead of 0 opacity when not expanded; tweak styling

--- src/components/general/DevKit.tsx
+++ src/components/general/DevKit.tsx
@@ -1,6 +1,6 @@
 import { Button } from "../ui/button";
 import { Link } from "react-router";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronsLeft, ChevronsRight } from "lucide-react";
 import useDevKit from "@/hooks/component-hooks/useDevKit";
 import { Input } from "../ui/input";
 
@@ -11,13 +11,7 @@ export default function DevKit() {
 
    return (
       <div className={`fixed bottom-4 left-4 transition-opacity ${loading ? "pointer-events-none opacity-70" : ""}`}>
-         <div
-            className={`mb-4 transition-all duration-100 ${
-               isExpanded ? "translate-x-0 opacity-100" : "translate-x-4 opacity-0"
-            } bg-foreground/10 rounded-lg border-2 border-gray-200 p-4 shadow-lg ${
-               isExpanded ? "pointer-events-auto" : "pointer-events-none"
-            }`}
-         >
+         <div className={` ${isExpanded ? "block" : "hidden"} rounded-lg border-2 border-white bg-black p-4 shadow-lg`}>
             <h3 className="mb-4 font-bold">Dev Kit</h3>
 
             <div className="mb-3">
@@ -45,12 +39,12 @@ export default function DevKit() {
             </div>
          </div>
 
-         <button
+         <Button
+            className="mt-3 size-12 rounded-full border-2 border-white bg-black text-white shadow-2xl hover:bg-black"
             onClick={() => setIsExpanded(!isExpanded)}
-            className="ring-foreground/10 flex h-9 w-9 items-center justify-center rounded-full bg-transparent text-white shadow-lg ring-1 ring-offset-2 outline-none hover:bg-gray-700"
          >
-            {isExpanded ? <ChevronLeft className="h-6 w-6" /> : <ChevronRight className="h-6 w-6" />}
-         </button>
+            {isExpanded ? <ChevronsLeft className="stroke-3" /> : <ChevronsRight className="stroke-3" />}
+         </Button>
       </div>
    );
 }
