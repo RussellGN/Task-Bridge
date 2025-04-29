f15fb40e79e152f78ffe05bc0d9808f8be93e5d0
Russellg <dudurussell@gmail.com>
2025-04-29 15:48:03 +0200

    fix: ensure project is defined before rendering dashboard

--- src/routes/ProjectDashboard.tsx
+++ src/routes/ProjectDashboard.tsx
@@ -10,12 +10,14 @@ export default function ProjectDashboard() {
    const { projectId } = useParams();
    const { project, isLoading, errorMessage } = useGetProject(projectId as string);
 
+   if (!project) return;
+
    return (
       <div className="flex h-full flex-col">
          <div className="w-full">
             <div className="mb-5 flex items-center gap-2">
                <BackBtn path={"/home?tab=all"} />
-               <h1 className="text-lg font-semibold italic">Project Dashboard/{project?.name || projectId}</h1>
+               <h1 className="text-lg font-semibold italic">Project Dashboard/{project.name || projectId}</h1>
 
                <div className="ml-auto">
                   <ProjectControls project={project} />
