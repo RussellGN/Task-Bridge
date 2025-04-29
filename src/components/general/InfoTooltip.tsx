56b19d16ee942e5c4aa22f0573b7dd7d36623985
Russellg <dudurussell@gmail.com>
2025-04-29 12:48:00 +0200

    feat: update InfoTooltip component to accept ReactNode for content and trigger props

--- src/components/general/InfoTooltip.tsx
+++ src/components/general/InfoTooltip.tsx
@@ -1,26 +1,40 @@
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
 import { Info, LucideIcon } from "lucide-react";
+import { ReactNode } from "react";
 
 type InfoTooltipProps = {
-   content: string;
+   content: ReactNode;
    className?: string;
    TriggerIcon?: LucideIcon;
+   trigger?: ReactNode;
    iconSize?: number;
    align?: "start" | "center" | "end";
    side?: "top" | "bottom" | "left" | "right";
 };
 
-export default function InfoTooltip({ content, className, TriggerIcon, iconSize, align, side }: InfoTooltipProps) {
+export default function InfoTooltip({
+   content,
+   className,
+   trigger,
+   TriggerIcon,
+   iconSize,
+   align,
+   side,
+}: InfoTooltipProps) {
    const Icon = TriggerIcon || Info;
    return (
       <TooltipProvider>
          <Tooltip>
             <TooltipTrigger>
-               <Icon
-                  size={iconSize || 18}
-                  className={cn("hover:text-PRIMARY text-PRIMARY/80 transition-colors", className)}
-               />
+               {trigger ? (
+                  trigger
+               ) : (
+                  <Icon
+                     size={iconSize || 18}
+                     className={cn("hover:text-PRIMARY text-PRIMARY/80 transition-colors", className)}
+                  />
+               )}
             </TooltipTrigger>
             <TooltipContent align={align} side={side}>
                <p>{content}</p>
