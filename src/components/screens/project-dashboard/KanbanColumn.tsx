d4aea530c75ca8d256ade75ab3836e12b74a2b1c
Russellg <dudurussell@gmail.com>
2025-05-02 10:52:35 +0200

    feat: lessen contrast of kanban columns

--- src/components/screens/project-dashboard/KanbanColumn.tsx
+++ src/components/screens/project-dashboard/KanbanColumn.tsx
@@ -15,13 +15,11 @@ type KanbanColumnProps = {
 
 export default function KanbanColumn({ project, title, Icon, tasks, newTaskForm, draftTasks }: KanbanColumnProps) {
    return (
-      <div className="bg-foreground/10 border-foreground flex h-full max-h-[85vh] flex-col gap-3 rounded-lg border p-4 shadow-lg">
+      <div className="bg-foreground/10 border-foreground/50 flex h-full max-h-[85vh] flex-col gap-3 rounded-lg border p-4 shadow-lg">
          <h2 className="flex items-center gap-2 border-b-2 pb-2 font-semibold">
-            <Icon className="text-PRIMARY stroke-3" />
+            <Icon />
             <span>{title}</span>
-            <span className="text-PRIMARY border-PRIMARY bg-background ml-auto block rounded-md border px-2 text-lg font-bold">
-               {tasks.length}
-            </span>
+            <span className="ml-auto">{tasks.length}</span>
          </h2>
 
          <div className="flex grow flex-col gap-3 overflow-y-auto pr-1">
