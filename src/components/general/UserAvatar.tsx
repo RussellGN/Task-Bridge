6ad511aea1469efbe4c1cb36c5fce3eba25e20af
Russellg <dudurussell@gmail.com>
2025-05-04 09:23:06 +0200

    feat: add default border to Avatar component

--- src/components/general/UserAvatar.tsx
+++ src/components/general/UserAvatar.tsx
@@ -1,9 +1,10 @@
 import { Author, User } from "@/types/interfaces";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { cn } from "@/lib/utils";
 
 export function UserAvatar({ user, className }: { user: User | Author; className?: string }) {
    return (
-      <Avatar className={className}>
+      <Avatar className={cn("border-foreground/50 border", className)}>
          <AvatarImage src={user.avatar_url} />
          <AvatarFallback>{user.login.charAt(0).toUpperCase()}</AvatarFallback>
       </Avatar>
