b589619badeb8f5010dc4f91124a951cc5b3127d
Russellg <dudurussell@gmail.com>
2025-05-15 10:26:37 +0200

    feat: replace RotateCw with LoaderCircle in SpinnerIcon component

--- src/components/general/SpinnerIcon.tsx
+++ src/components/general/SpinnerIcon.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
-import { RotateCw } from "lucide-react";
+import { LoaderCircle } from "lucide-react";
 
 export default function SpinnerIcon({ className }: { className?: string }) {
-   return <RotateCw className={cn("animate-spin", className)} />;
+   return <LoaderCircle className={cn("animate-spin", className)} />;
 }
