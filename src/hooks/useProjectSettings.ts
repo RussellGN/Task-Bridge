d489f4276aba72b8e797f7aec466d82892322932
Russellg <dudurussell@gmail.com>
2025-07-01 12:44:03 +0200

    fix: handle uncaught project settings update errors

--- src/hooks/useProjectSettings.ts
+++ src/hooks/useProjectSettings.ts
@@ -24,27 +24,31 @@ export default function useProjectSettings() {
       dbg("[updateProjectSettings] Updating project settings", settings_patch);
 
       (async () => {
-         if (settings_patch.permanent_delete_project) {
-            if (settings_patch.permanent_delete_project === `delete ${project?.name}`) deleteProjectPermanently();
-            else alertError(`${F} Delete instruction does not match expected format`);
-            return;
+         try {
+            if (settings_patch.permanent_delete_project) {
+               if (settings_patch.permanent_delete_project === `delete ${project?.name}`) deleteProjectPermanently();
+               else alertError(`${F} Delete instruction does not match expected format`);
+               return;
+            }
+
+            if (settings_patch.locally_delete_project) {
+               if (settings_patch.locally_delete_project === `delete ${project?.name}`) deleteProjectLocally();
+               else alertError(`${F} Delete instruction does not match expected format`);
+               return;
+            }
+
+            const patchArgs = { project_id: project!.id, settings_patch: settings_patch } as ProjectPatchArgs;
+
+            if (settings_patch.name && settings_patch.repo_name && settings_patch.repo_visibility) {
+               await invoke("update_general_project_metadata", { patchArgs });
+            }
+
+            if (settings_patch.team) await invoke("update_project_team", { patchArgs });
+
+            if (settings_patch.project_sync_interval_mins) await updateProjectSyncSettings(patchArgs);
+         } catch (error) {
+            alertError(`${F} Error updating project settings: ${error}`);
          }
-
-         if (settings_patch.locally_delete_project) {
-            if (settings_patch.locally_delete_project === `delete ${project?.name}`) deleteProjectLocally();
-            else alertError(`${F} Delete instruction does not match expected format`);
-            return;
-         }
-
-         const patchArgs = { project_id: project!.id, settings_patch: settings_patch } as ProjectPatchArgs;
-
-         if (settings_patch.name && settings_patch.repo_name && settings_patch.repo_visibility) {
-            await invoke("update_general_project_metadata", { patchArgs });
-         }
-
-         if (settings_patch.team) await invoke("update_project_team", { patchArgs });
-
-         if (settings_patch.project_sync_interval_mins) await updateProjectSyncSettings(patchArgs);
       })();
    }
 
