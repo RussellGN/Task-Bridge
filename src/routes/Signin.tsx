c20368733aca79430c372049de6dc864acb0b29a
Russellg <dudurussell@gmail.com>
2025-07-02 08:30:31 +0200

    feat: tweak sign in screen UI

--- src/routes/Signin.tsx
+++ src/routes/Signin.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/components/general/Spinner";
 import { Button } from "@/components/ui/button";
 import useSignin from "@/hooks/route-hooks/useSignin";
 import { GITHUB_AUTH_URL, GITHUB_INSTALL_URL } from "@/lib/constants";
-import { AlertTriangle, LogIn, UserCircle } from "lucide-react";
+import { AlertCircle, LogIn, UserCircle } from "lucide-react";
 import { Link } from "react-router";
 
 export default function Signin() {
@@ -12,11 +12,11 @@ export default function Signin() {
       <div className="flex flex-col items-center justify-center gap-3 pt-30 text-center">
          <UserCircle className="lucide-exempt" size={100} />
 
-         <p>You need to allow integration with your GitHub account in order to proceed</p>
+         <p>Signin to GitHub</p>
 
          <p className="text-foreground/70 mb-3 max-w-prose text-sm">
-            <AlertTriangle size={13} className="-mt-0.5 mr-1 inline-block" />
-            This is will enable the app to interact with GitHub <br /> on behalf of your account
+            <AlertCircle size={13} className="lucide-exempt -mt-0.5 mr-1 inline-block" />
+            GitHub Integration is required to use Task-bridge
          </p>
 
          <span className={loading ? "cursor-not-allowed" : ""}>
@@ -27,18 +27,18 @@ export default function Signin() {
             >
                <Link to={loading ? "" : GITHUB_INSTALL_URL} target={loading ? "" : "_blank"}>
                   {loading && <Spinner size="sm" />}
-                  Integrate Github Account
+                  Sign In
                   <LogIn />
                </Link>
             </Button>
          </span>
 
-         <div className="">- or -</div>
+         <div className="mt-10">or</div>
 
          <Link
             to={loading ? "" : GITHUB_AUTH_URL}
             target={loading ? "" : "_blank"}
-            className={loading ? "" : "text-PRIMARY underline"}
+            className={loading ? "" : "text-PRIMARY"}
          >
             Check GitHub for previous <br /> integration
          </Link>
