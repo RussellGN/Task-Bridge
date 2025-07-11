7486eed01abf4e5892e0c2612aedd16d3ae34b4f
Russellg <dudurussell@gmail.com>
2025-07-11 16:53:54 +0200

    fix: reconfigure single instance dep; modify plugin init order

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,11 +18,11 @@ pub type Result<T = ()> = std::result::Result<T, String>;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
    tauri::Builder::default()
-      .plugin(tauri_plugin_http::init())
       .plugin(tauri_plugin_single_instance::init(init_tauri_plugin_single_instance))
       .plugin(tauri_plugin_deep_link::init())
       .plugin(tauri_plugin_store::Builder::new().build())
       .plugin(tauri_plugin_opener::init())
+      .plugin(tauri_plugin_http::init())
       .setup(setup::setup)
       .invoke_handler(tauri::generate_handler![
          commands::fetch_save_and_return_user,
