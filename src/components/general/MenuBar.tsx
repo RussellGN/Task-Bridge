61955a38d820382f123733a31f7f18328ae15072
Russellg <dudurussell@gmail.com>
2025-07-12 14:11:06 +0200

    feat: display UI note on new git branch naming convention

--- src/components/general/MenuBar.tsx
+++ src/components/general/MenuBar.tsx
@@ -1,9 +1,15 @@
+import { Info } from "lucide-react";
 import { AccountManager } from "./AccountManager";
 
 export default function MenuBar() {
    return (
       <div className="flex px-3 md:px-5 lg:px-8">
          <AccountManager />
+         <div className="text-WARNING flex grow items-center justify-center gap-2 text-center text-sm">
+            <Info />
+            Developers should append <b>{"_<task number>"}</b> to the names of git branches to allow tracking of task
+            activity e.g <b>{"feature/reports_23"}</b>
+         </div>
       </div>
    );
 }
