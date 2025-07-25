a4e443f9d552b02adf4ed75f9cbc53a37b6651e6
Russellg <dudurussell@gmail.com>
2025-07-25 08:43:25 +0200

    style: tweak menu-bar styling

--- src/components/general/MenuBar.tsx
+++ src/components/general/MenuBar.tsx
@@ -1,9 +1,39 @@
+import {
+   Menubar,
+   MenubarContent,
+   MenubarItem,
+   MenubarMenu,
+   MenubarSeparator,
+   MenubarShortcut,
+   MenubarTrigger,
+} from "@/components/ui/menubar";
 import { AccountManager } from "./AccountManager";
 
 export default function MenuBar() {
    return (
-      <div className="flex px-3 md:px-5 lg:px-8">
-         <AccountManager />
-      </div>
+      <>
+         <div className="bg-DANGER flex px-3 md:px-5 lg:px-8">
+            <div className="bg-SUCCESS">
+               <AccountManager />
+            </div>
+         </div>
+
+         <Menubar className="bg-PRIMARY">
+            <MenubarMenu>
+               <MenubarTrigger>File</MenubarTrigger>
+
+               <MenubarContent>
+                  <MenubarItem>
+                     New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                  </MenubarItem>
+                  <MenubarItem>New Window</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem>Share</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem>Print</MenubarItem>
+               </MenubarContent>
+            </MenubarMenu>
+         </Menubar>
+      </>
    );
 }
