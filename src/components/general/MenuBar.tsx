9b92df87ea1a3dc4a52f04bd196e619d878af3eb
Russellg <dudurussell@gmail.com>
2025-07-25 11:45:10 +0200

    feat: add Home and Settings links to MenuBar

--- src/components/general/MenuBar.tsx
+++ src/components/general/MenuBar.tsx
@@ -1,14 +1,20 @@
 import { Menubar } from "@/components/ui/menubar";
 import { AccountManager } from "./AccountManager";
 import MenuBarLinkItem from "./MenuBarLinkItem";
-import { HelpCircle } from "lucide-react";
+import { HelpCircle, HomeIcon, Settings } from "lucide-react";
 
 export default function MenuBar() {
    return (
       <Menubar>
+         <MenuBarLinkItem to="/home" Icon={HomeIcon}>
+            Home
+         </MenuBarLinkItem>
          <AccountManager />
          <MenuBarLinkItem to="/help" Icon={HelpCircle}>
             Help
+         </MenuBarLinkItem>
+         <MenuBarLinkItem to="/settings" Icon={Settings}>
+            Settings
          </MenuBarLinkItem>
       </Menubar>
    );
