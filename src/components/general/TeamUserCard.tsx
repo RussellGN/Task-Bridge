7a7f1310b371ecf4133f08a7e1aa94ac92e20221
russellgn <dudurussell@gmail.com>
2025-08-05 11:31:58 +0200

    style: adjust avatar styling

--- src/components/general/TeamUserCard.tsx
+++ src/components/general/TeamUserCard.tsx
@@ -14,7 +14,7 @@ type TeamUserCardProps = {
 
 export default function TeamUserCard({ user, pending, className, showPendingState, onRemove }: TeamUserCardProps) {
    return (
-      <div className={cn("bg-foreground/5 flex items-center gap-2 rounded-sm border px-2 py-0.5 text-xs", className)}>
+      <div className={cn("bg-card flex items-center gap-2 rounded-sm border px-2 py-0.5 text-xs", className)}>
          <UserAvatar
             user={user}
             className={`size-6 ${showPendingState ? "border-3" : "border-0"} ${pending ? "border-[grey]" : "border-PRIMARY"}`}
--- src/components/general/UserAvatar.tsx
+++ src/components/general/UserAvatar.tsx
@@ -4,7 +4,7 @@ import { cn } from "@/lib/utils";
 
 export function UserAvatar({ user, className }: { user: Author; className?: string }) {
    return (
-      <Avatar className={cn("border-foreground/50 border", className)}>
+      <Avatar className={cn("bg-card border-foreground/30 border", className)}>
          <AvatarImage src={user.avatar_url} />
          <AvatarFallback className="">{user.login.charAt(0).toUpperCase()}</AvatarFallback>
       </Avatar>
