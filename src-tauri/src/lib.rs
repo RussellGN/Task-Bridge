fdef94e9d16a73247279ae8561965d3570349f53
russellgn <dudurussell@gmail.com>
2025-08-09 16:44:25 +0200

    fix: disable single instance plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,7 +18,7 @@ pub type Result<T = ()> = std::result::Result<T, String>;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
    tauri::Builder::default()
-      .plugin(tauri_plugin_single_instance::init(init_tauri_plugin_single_instance))
+      // .plugin(tauri_plugin_single_instance::init(init_tauri_plugin_single_instance))
       .plugin(tauri_plugin_deep_link::init())
       .plugin(tauri_plugin_store::Builder::new().build())
       .plugin(tauri_plugin_opener::init())
