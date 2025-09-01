e1a35f634df79630f49d13d7070441cfdc5e8f14
russellgn <dudurussell@gmail.com>
2025-09-01 09:06:04 +0200

    fix: welcome screen handling

--- src/hooks/route-hooks/useWelcome.ts
+++ src/hooks/route-hooks/useWelcome.ts
@@ -1,30 +1,33 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { checkAuth } from "@/lib/utils";
 import { useNavigate, useSearchParams } from "react-router";
 import useGetUser from "../backend-api-hooks/internet-independant/useGetUser";
 import useAppPreferences from "../useAppPreferences";
 
 export default function useWelcome() {
-   const { appPreferences, appPreferencesloading, updateAppPreferencesManually } = useAppPreferences();
+   const [isFirstMount, setIsFirstMount] = useState(true);
+   const { appPreferences, updateAppPreferencesManually } = useAppPreferences();
    const { user, error, loading, getUser } = useGetUser();
    const [searchParams, setSearchParams] = useSearchParams();
    const activeTab = searchParams.get("tab") || "home";
    const navigate = useNavigate();
 
    useEffect(() => {
-      if (appPreferencesloading) return;
       // checkAuth should not raise an error, crash if it does
       checkAuth().then((isAuthed) => {
          if (!isAuthed) {
             // alertError("[useHome] You are not authenticated. Please sign in.");
             navigate("/");
          } else {
-            if (appPreferences?.is_not_first_launch) navigate("/projects");
-            else updateAppPreferencesManually({ is_not_first_launch: true });
+            if (appPreferences?.is_not_first_launch && isFirstMount) navigate("/projects");
+            else {
+               setIsFirstMount(false);
+               updateAppPreferencesManually({ is_not_first_launch: true });
+            }
             getUser();
          }
       });
-   }, [appPreferences, appPreferencesloading]);
+   }, [appPreferences?.is_not_first_launch]);
 
    function setActiveTab(tab: string) {
       setSearchParams((prev) => {
