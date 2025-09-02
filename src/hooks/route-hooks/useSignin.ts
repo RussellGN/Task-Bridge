7395d6dc1285a2868c02832ce8c6ab7db1e359dc
russellgn <dudurussell@gmail.com>
2025-09-02 12:21:47 +0200

    fix: remove window focus reload on signin

--- src/hooks/route-hooks/useSignin.ts
+++ src/hooks/route-hooks/useSignin.ts
@@ -23,17 +23,17 @@ export default function useSignin() {
       })();
    }, []);
 
-   useEffect(() => {
-      const handleFocus = () => {
-         window.location.reload();
-      };
+   // useEffect(() => {
+   //    const handleFocus = () => {
+   //       window.location.reload();
+   //    };
 
-      window.addEventListener("focus", handleFocus);
+   //    window.addEventListener("focus", handleFocus);
 
-      return () => {
-         window.removeEventListener("focus", handleFocus);
-      };
-   }, []);
+   //    return () => {
+   //       window.removeEventListener("focus", handleFocus);
+   //    };
+   // }, []);
 
    return { loading };
 }
