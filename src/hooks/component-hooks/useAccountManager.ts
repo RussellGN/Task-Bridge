f68ff9613d6c2b856013c4dd62f8eb959fa2177a
russellgn <dudurussell@gmail.com>
2025-09-02 12:52:52 +0200

    fix: fetch user data on account manager initialization

--- src/hooks/component-hooks/useAccountManager.ts
+++ src/hooks/component-hooks/useAccountManager.ts
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useGetUser from "../backend-api-hooks/internet-independant/useGetUser";
 import useSignout from "../backend-api-hooks/internet-independant/useSignout";
 
 export default function useAccountManager() {
-   const { user, loading: userLoading } = useGetUser();
+   const { user, loading: userLoading, getUser } = useGetUser();
    const { isPending: isSigningOut, signout } = useSignout();
    const [showSignoutDialog, setShowSignoutDialog] = useState(false);
+
+   useEffect(() => {
+      getUser();
+   }, []);
 
    return {
       user,
