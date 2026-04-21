63cca6a231eacc2f2a90c0d97a66b066d00874d0
russellgn <dudurussell@gmail.com>
2026-04-21 12:15:03 +0200

    fix: website config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-   // base: process.env.GITHUB_ACTIONS ? "/task-bridge/" : "/",
+   base: process.env.GITHUB_ACTIONS ? "/Task-Bridge/" : "/",
    plugins: [tailwindcss()],
 });
